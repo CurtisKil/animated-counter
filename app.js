852eade95d5d56c5a07d943916585270ef26c846
lucidck <curtiskilgore@hotmail.com>
2020-12-03 15:59:33 -0800

    Counter JS

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const counters = document.querySelectorAll(".counter");
+const speed = 500;
 
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const increment = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
